fca85c6404cd7fe39d73e3c52c758e185cec039f
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-04-20 19:18:27 +0300

    initial client files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
