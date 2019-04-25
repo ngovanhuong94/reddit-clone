41e51d4b9ff573bd6c0734adbd450ddf1a9f8031
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-04-25 19:42:57 +0300

    user model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
+// modules
 const express = require('express')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+// config environment
 dotenv.config()
 
+// connect database
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+.then(() => console.log('Connected to database'))
+.catch(() => console('Failed to connect database'))
+
+// create app
 const app = express()
 
 const PORT = process.env.PORT || 5000
+// run server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false }
+})
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+} 
+
+module.exports = mongoose.model('User', userSchema)
